46a06533178b9f1d1b2a51608beb2067242edf37
Dennis Veeneman <dennis_veeneman@hotmail.com>
2017-03-08 22:16:39 +0100

    kleinere svg

--- resources/views/rental-agreement.blade.php
+++ resources/views/rental-agreement.blade.php
@@ -8,7 +8,7 @@
     <link href="/css/pdf.css" rel="stylesheet">
 </head>
 <body>
-    <img src="{{ env('APP_URL', 'http://localhost') }}/img/ijsselgroep-logo.svg" alt="Scouting IJsselgroep Gorssel">
+    <img src="{{ env('APP_URL', 'http://localhost') }}/img/ijsselgroep-logo.svg" alt="Scouting IJsselgroep Gorssel" width="5cm" style="float:left;">
 	<h1>Overeenkomst</h1>
 </body>
 </html>
